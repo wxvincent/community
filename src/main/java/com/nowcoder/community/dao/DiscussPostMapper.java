bb9ccec843345a5e68a01df980eb20567cdccecd
itwx <942774363@qq.com>
2019-08-12 10:30:39 +0800

    8/12上午提交

---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,16 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+
+    //使用了动态sql（<if>）,必须加@Param注解
+    List<DiscussPost> selectDiscussPosts(@Param("userId") int userId, @Param("offset") int offset, @Param("limit") int limit);
+
+    int selectDiscussPostRows(@Param("userId") int userId);
+}
