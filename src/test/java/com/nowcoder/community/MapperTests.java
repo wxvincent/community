5a5df6f27fc56dda3cf3310643f14175532bd08d
itwx <942774363@qq.com>
2019-08-17 19:53:54 +0800

    8/17下午提交（ThreadLocal）

--- src/test/java/com/nowcoder/community/MapperTests.java
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -1,8 +1,10 @@
 package com.nowcoder.community;
 
 import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.LoginTicketMapper;
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.LoginTicket;
 import com.nowcoder.community.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -24,8 +27,11 @@ public class MapperTests {
     @Autowired
     private DiscussPostMapper discussPostMapper;
 
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
+
     @Test
-    public void test1(){
+    public void test1() {
         User user = userMapper.selectById(101);
         System.out.println(user);
 
@@ -34,12 +40,33 @@ public class MapperTests {
     }
 
     @Test
-    public void testDiscussPost(){
+    public void testDiscussPost() {
         List<DiscussPost> list = discussPostMapper.selectDiscussPosts(0, 0, 10);
         for (DiscussPost post : list) {
             System.out.println(post);
         }
         int i = discussPostMapper.selectDiscussPostRows(0);
         System.out.println(i);
+    }
+
+    @Test
+    public void testLoginTicket() {
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(123);
+        loginTicket.setTicket("abc");
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000 * 60 * 10));
+
+        loginTicketMapper.insertLoginTicket(loginTicket);
+    }
+
+    @Test
+    public void testSelectMapper() {
+        LoginTicket loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+
+        loginTicketMapper.updateStatus("abc", 1);
+        loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
     }
 }
