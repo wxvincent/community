a792eb07d569f0f3d546304d0e8652ff5bf30406
itwx <942774363@qq.com>
2019-08-26 19:21:06 +0800

    8/26下午提交（完成添加评论功能）

--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -27,7 +27,7 @@ public class DiscussPostService {
     }
 
     public int addDiscussPost(DiscussPost post) {
-        if (post==null) {
+        if (post == null) {
             throw new IllegalArgumentException("参数不能为空！");
         }
 
@@ -43,5 +43,9 @@ public class DiscussPostService {
 
     public DiscussPost findDiscussPostById(int id) {
         return discussPostMapper.selectDiscussPostById(id);
+    }
+
+    public int updateCommentCount(int id, int commentCount) {
+        return discussPostMapper.updateCommentCount(id, commentCount);
     }
 }
