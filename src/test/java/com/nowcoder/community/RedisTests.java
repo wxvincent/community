30486f12abf5389ff3a1f5a0347c5a11a0ae171f
itwx <942774363@qq.com>
2019-08-29 11:32:14 +0800

    8/28下午提交（完成点赞功能）

---
+++ src/test/java/com/nowcoder/community/RedisTests.java
@@ -0,0 +1,27 @@
+package com.nowcoder.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class RedisTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void test() {
+        String redisKey = "test:redis";
+        redisTemplate.opsForValue().set(redisKey, 1);
+        System.out.println(redisTemplate.opsForValue().get(redisKey));
+        System.out.println(redisTemplate.opsForValue().increment(redisKey));
+
+    }
+}
