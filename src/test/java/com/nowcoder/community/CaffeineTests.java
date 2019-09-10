3b68d4ded8f5df08c3f6b2bba417279560efb6f1
itwx <942774363@qq.com>
2019-09-10 20:50:34 +0800

    9/10晚上提交（完成使用caffeine优化网站性能功能）

---
+++ src/test/java/com/nowcoder/community/CaffeineTests.java
@@ -0,0 +1,42 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.service.DiscussPostService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class CaffeineTests {
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Test
+    public void initDataForTest() {
+        for (int i = 0; i < 300000; i++) {
+            DiscussPost discussPost = new DiscussPost();
+            discussPost.setUserId(111);
+            discussPost.setTitle("互联网求职暖春计划");
+            discussPost.setContent("今年的就业形势，确实不容乐观，过了个年，仿佛跳水一般，整个评论区哀鸿遍野！！");
+            discussPost.setCreateTime(new Date());
+            discussPost.setScore(Math.random() * 2000);
+            discussPostService.addDiscussPost(discussPost);
+        }
+    }
+
+    @Test
+    public void testCache() {
+        System.out.println(discussPostService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(discussPostService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(discussPostService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(discussPostService.findDiscussPosts(0, 0, 10, 0));
+    }
+}
